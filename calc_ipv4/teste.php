6b7cf4f2d9ed6d641dd2ed0a8f97d5b40e30b305
Jessica <jessica.viana111700@gmail.com>
2018-07-11 14:12:31 -0300

    Add files via upload

---
+++ calc_ipv4/teste.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 11/07/18
+ * Time: 14:01
+ */
+
+$regexp = '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/';
+
+echo $regexp;
