1da8a02f3eec50e884134780d1627870f9312454
Jessica <jessica.viana111700@gmail.com>
2018-07-12 14:27:57 -0300

    Add files via upload

--- calc_ipv4/index.php
+++ calc_ipv4/index.php
@@ -1,26 +1,27 @@
-<?php include 'app/classes/class_calc_ip.php'; ?>
+<?php include 'app/classes/calculo.php'; ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-
+     <link rel="stylesheet" href="uni.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <meta charset="UTF-8">
     <title>Cálculo de máscara de sub-rede IPv4</title>
 
 </head>
-<body>
-    <section>
-        <h1>Calcular máscara de sub-rede IPv4</h1>
+<body id="sugar">
+
+    <section >
+        <h1 id="bee" >Calcular máscara de sub-rede IPv4</h1>
 
     <!-- Formulário Verde p pegar ip-->
     <form method="POST">
-        <b style="color: green">IP/CIDR</b> <small>(Ex.: 192.168.0.1/24)</small> <br> 
-        <input style="border:1px solid green; line-height: 2; padding: 0 5px; width: 200px;" type="text" name="ip" value="<?php echo @$_POST['ip'];?>">
-        <input style="border:1px solid green; background: green; color: #fff; font-weight: 700; cursor: pointer; line-height: 2; padding: 0 5px;" type="submit" value="Calcular">
+        <b id="moon" >IP/CIDR</b> (Ex.: 192.168.0.1/24) <br>
+        <input id="star" type="text" name="ip" value="<?php echo @$_POST['ip'];?>">
+        <input id="constellation"  type="submit" value="Calcular">
     </form>
 <?php
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' && ! empty( $_POST['ip'] ) ) {
-    $ip = new calc_ip( $_POST['ip'] );
+    $ip = new calculoIp( $_POST['ip'] );
     
     if( $ip->valida_end() ) {
         echo '<h2>Configurações de rede para <span style="color:gray;">' . $_POST['ip'] . '</span> </h2>';
@@ -28,9 +29,9 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' && ! empty( $_POST['ip'] ) ) {
         
         echo "<b>Endereço/Rede: </b>" . $ip->endereco_completo() . '<br>';
         echo "<b>Endereço: </b>" . $ip->endereco() . '<br>';
-        echo "<b>Prefixo CIDR: </b>/" . $ip->cidr() . '<br>';
+        echo "<b>Prefixo CIDR: </b>/" . $ip->barramento() . '<br>';
         echo "<b>Máscara de sub-rede: </b>" . $ip->mascara() . '<br>';
-        echo "<b>IP da Rede: </b>" . $ip->rede() . '/' . $ip->cidr() . '<br>';
+        echo "<b>IP da Rede: </b>" . $ip->rede() . '/' . $ip->b() . '<br>';
         echo "<b>Broadcast da Rede: </b>" . $ip->broadcast() . '<br>';
         echo "<b>Primeiro Host: </b>" . $ip->primeiro_ip() . '<br>';
         echo "<b>Último Host: </b>" . $ip->ultimo_ip() . '<br>';
--- calc_ipv4/teste.php
+++ calc_ipv4/teste.php
@@ -6,6 +6,16 @@
  * Time: 14:01
  */
 
-$regexp = '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/';
+$regexp = '/^[0-9]{1,3}\.[0-9]{1,3}/';
+$end    = "123.234";
 
-echo $regexp;
+
+//echo preg_match( $regexp, $end);
+
+//192.2.0.1/24
+//.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/
+
+$ip = gethostbyname('www.example.com');
+$out = "The following URLs are equivalent:\n";
+$out .= 'http://www.example.com/, http://' . $ip . '/, and http://' . sprintf("%u", ip2long($ip)) . "\n";
+echo $out;
